f59f4c140599f139f9c197c7f0072740ba064b51
ezio4518 <mdgufran4518@gmail.com>
2024-06-06 22:42:23 +0530

    feat: Add Cart, PlaceOrder, Home components and their styles

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Lorem ipsum dolor adipisicing elit.</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod soluta ad iusto, inventore similique accusantium suscipit totam temporibus ut vitae praesentium officia voluptates ullam dolores laudantium sunt, error consequuntur deserunt.</p>
+            <button>Shop Now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
