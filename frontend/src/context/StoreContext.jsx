f8c2f23dfb99065a1e75be9329f9f2aad003f1f6
ezio4518 <mdgufran4518@gmail.com>
2024-06-09 12:08:31 +0530

    chore: Remove unused useEffect in StoreContextProvider

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
@@ -30,10 +30,6 @@ const StoreContextProvider = (props) => {
         }
         return totalAmount;
     }
-
-    // useEffect(()=>{
-    //     console.log(cartItems);
-    // },[cartItems])
 
     const contextValue = {
         food_list,
