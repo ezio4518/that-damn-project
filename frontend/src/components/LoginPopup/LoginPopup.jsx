c6650437c8415e6854847f817bf194704b412381
ezio4518 <mdgufran4518@gmail.com>
2024-07-05 16:43:23 +0530

    chore: Update Add page to use "add-product-name" class instead of "add-product-upload"

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -5,6 +5,17 @@ import { assets } from '../../assets/assets'
 const LoginPopup = ({setShowLogin}) => {
 
   const [currState,setCurrState] = useState("Login")
+  const [data,setData] = useState({
+    name:"",
+    email:"",
+    password:""
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
 
   return (
     <div className='login-popup'>
