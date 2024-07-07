e6953d41f5b4c8b972775323559c4d07618638ad
ezio4518 <mdgufran4518@gmail.com>
2024-07-07 20:32:58 +0530

    chore: Update Verify page CSS and add verifyOrder, userOrders, and listOrders functions to orderController

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { placeOrder } from '../controllers/orderController.js';
+import { listOrders, placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js';
 
 const oerderRouter = express.Router();
 
 orderRouter.post('/place', authMiddleware, placeOrder);
+orderRouter.post("/verify",verifyOrder);
+orderRouter.post("/userorders",authMiddleware,userOrders)
+orderRouter.get('/list',listOrders)
 
 export default orderRouter;
