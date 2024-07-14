93a16feb5a71d42de50926cdcf465537d72918a4
ezio4518 <mdgufran4518@gmail.com>
2024-07-15 02:02:20 +0530

    chore: Update MongoDB connection string to use environment variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 //isko safe krna h
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ezio:Myy110998@cluster0.vz623uu.mongodb.net/that-damn-project').then(()=>console.log("DB connected"));
+    await mongoose.connect(`${process.env.MONGODB_URI}/that-damn-project`).then(()=>console.log("DB connected"));
 }
 
