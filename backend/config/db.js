808f8b54b687ac0f40793729bcfb1f93e3c9354e
ezio4518 <mdgufran4518@gmail.com>
2024-07-14 14:18:53 +0530

    chore: Update CSS and functionality for order status in Orders page

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+//isko safe krna h
 export const connectDB = async () => {
     await mongoose.connect('mongodb+srv://ezio:Myy110998@cluster0.vz623uu.mongodb.net/that-damn-project').then(()=>console.log("DB connected"));
 }
